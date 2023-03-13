8c74007eaca672702dacc7c03e9ecf81a0895183
PJLys <pjlyssens@gmail.com>
2023-03-13 09:02:50 +0100

    Initial commit

---
+++ src/main/java/dist/group2/NamingServer/NamingServerApplication.java
@@ -0,0 +1,13 @@
+package dist.group2.NamingServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerApplication.class, args);
+	}
+
+}
